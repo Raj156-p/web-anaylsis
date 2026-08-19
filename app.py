dccb83c7cac3118089b0e7dd83fa4430660be4b1
Raj156-p <rajsuhagya@gmail.com>
2026-08-19 18:01:36 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def user_orders(user_id):
         except:pass
     return jsonify({'orders':rows})
 
-if __name__=='__main__': app.run(host='0.0.0.0',port=5000,debug=True)
+if __name__=='__main__': app.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
